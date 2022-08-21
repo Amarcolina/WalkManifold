3e95220a5ce42ffa93a1e34e0600f9e8e24ae9e8
Gerald Krystian <amarcolina@gmail.com>
2022-08-21 00:10:00 -0700

    Add basic damping the example player character

--- Assets/Scripts/ExamplePlayerController.cs
+++ Assets/Scripts/ExamplePlayerController.cs
@@ -11,14 +11,18 @@ public class ExamplePlayerController : MonoBehaviour {
   public float SprintSpeed;
   public float MouseSensitivity;
   public Transform Head;
+  [Range(0, 1)]
+  public float HeadDamping = 0.95f;
 
   private ManifoldCharacterController _controller;
   private CharacterController _cc;
   private Vector3 _rotation;
+  private Vector3 _headOrigin;
 
   private void Awake() {
     _controller = GetComponent<ManifoldCharacterController>();
     _cc = GetComponent<CharacterController>();
+    _headOrigin = Head.localPosition;
   }
 
   private void OnEnable() {
@@ -33,10 +37,12 @@ public class ExamplePlayerController : MonoBehaviour {
 
   private void LateUpdate() {
     if (Time.time < 1) return;
-    if (!Application.isFocused) return;
 
     Vector3 moveDir = transform.right * Input.GetAxis("Horizontal") +
                       transform.forward * Input.GetAxis("Vertical");
+
+    Vector3 prevHeadPos = Head.position;
+    Head.localPosition = _headOrigin;
 
     if (UseCharacterController) {
       _cc.SimpleMove(moveDir * (Input.GetKey(KeyCode.LeftShift) ? SprintSpeed : WalkSpeed));
@@ -44,9 +50,13 @@ public class ExamplePlayerController : MonoBehaviour {
       _controller.SimpleMove(moveDir * (Input.GetKey(KeyCode.LeftShift) ? SprintSpeed : WalkSpeed));
     }
 
-    transform.Rotate(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0);
+    Head.position = Vector3.Lerp(prevHeadPos, Head.position, 1.0f - HeadDamping);
 
-    _rotation.x = Mathf.Clamp(_rotation.x - Input.GetAxis("Mouse Y") * MouseSensitivity, -85, 85);
-    Head.localEulerAngles = _rotation;
+    if (Application.isFocused) {
+      transform.Rotate(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0);
+
+      _rotation.x = Mathf.Clamp(_rotation.x - Input.GetAxis("Mouse Y") * MouseSensitivity, -85, 85);
+      Head.localEulerAngles = _rotation;
+    }
   }
 }
