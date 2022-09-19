049d22b4a0ed96d947b742cf9eb1fbdc54ee5c1a
Alex Marcolina <alex.marcolina@oculus.com>
2022-09-19 16:52:44 -0700

    Small tweaks to partial ring generation job
    Change from switch to lookup table for type calculation
    Switch to using sorting network as only option to reduce clutter
    Various code cleanup / tweaking

--- Packages/com.walkmanifold/Runtime/Scripts/Jobs/CreatePartialRingsJob.cs
+++ Packages/com.walkmanifold/Runtime/Scripts/Jobs/CreatePartialRingsJob.cs
@@ -1,6 +1,3 @@
-#define USE_SORTING_NETWORK
-
-using UnityEngine;
 using Unity.Jobs;
 using Unity.Burst;
 using Unity.Mathematics;
@@ -13,11 +10,30 @@ namespace WalkManifold.Internals {
   public struct CreatePartialRingsJob : IJob {
 
     public static readonly bool USE_SORTING_NETWORK = true;
-    public static readonly int2[] LocalCoordinates = new int2[] {
+    public static readonly int2[] LocalCoordinates = {
       new int2(0, 0),
       new int2(1, 0),
       new int2(1, 1),
       new int2(0, 1)
+    };
+
+    public static readonly PartialRingType[] MaskToPartialTypeLookup = {
+      /* 0000 */ PartialRingType.Invalid,
+      /* 0001 */ PartialRingType.Corner,
+      /* 0010 */ PartialRingType.Corner,
+      /* 0011 */ PartialRingType.Edge,
+      /* 0100 */ PartialRingType.Corner,
+      /* 0101 */ PartialRingType.Diagonal,
+      /* 0110 */ PartialRingType.Edge,
+      /* 0111 */ PartialRingType.InvertedCorner,
+      /* 1000 */ PartialRingType.Corner,
+      /* 1001 */ PartialRingType.Edge,
+      /* 1010 */ PartialRingType.Diagonal,
+      /* 1011 */ PartialRingType.InvertedCorner,
+      /* 1100 */ PartialRingType.Edge,
+      /* 1101 */ PartialRingType.InvertedCorner,
+      /* 1110 */ PartialRingType.InvertedCorner,
+      /* 1111 */ PartialRingType.Complete,
     };
 
     [ReadOnly]
@@ -39,12 +55,6 @@ namespace WalkManifold.Internals {
       var poles = new NativeArray<PoleWithCorner>(4, Allocator.Temp);
       var rings = new NativeArray<int>(4, Allocator.Temp);
 
-#if !USE_SORTING_NETWORK
-      var comparer = new PoleComparer() {
-        Vertices = Vertices
-      };
-#endif
-
       for (int x = Min.x; x < Max.x; x++) {
         for (int y = Min.y; y < Max.y; y++) {
           poles[0] = CellToPoleIndex[new int2(x + 0, y + 0)].AtCorner(0);
@@ -53,10 +63,10 @@ namespace WalkManifold.Internals {
           poles[3] = CellToPoleIndex[new int2(x + 0, y + 1)].AtCorner(3);
 
           while (true) {
-#if USE_SORTING_NETWORK
-            //Not really a super important optimization considering the cost of other operations
-            //in this package, but it sure was fun to implement!
+            //Use a sorting network to sort the poles in order based on the height of their highest points
             {
+              //Sorting network compared to a regular sort is not a super important optimization, but it
+              //sure was fun to implement!
               float4 heights = new float4(poles[0].Count == 0 ? float.MinValue : Vertices[poles[0].Start].y,
                                           poles[1].Count == 0 ? float.MinValue : Vertices[poles[1].Start].y,
                                           poles[2].Count == 0 ? float.MinValue : Vertices[poles[2].Start].y,
@@ -72,22 +82,18 @@ namespace WalkManifold.Internals {
               indices = select(indices, indices.yxwz, compB);
 
               bool compC = heights.y > heights.z;
-              heights = select(heights, heights.xzyw, compC);
               indices = select(indices, indices.xzyw, compC);
 
-              var p0 = poles[(int)indices.x];
-              var p1 = poles[(int)indices.y];
-              var p2 = poles[(int)indices.z];
-              var p3 = poles[(int)indices.w];
+              var p0 = poles[(int)indices.w];
+              var p1 = poles[(int)indices.z];
+              var p2 = poles[(int)indices.y];
+              var p3 = poles[(int)indices.x];
 
-              poles[0] = p3;
-              poles[1] = p2;
-              poles[2] = p1;
-              poles[3] = p0;
+              poles[0] = p0;
+              poles[1] = p1;
+              poles[2] = p2;
+              poles[3] = p3;
             }
-#else
-            poles.Sort(comparer);
-#endif
 
             //No more poles left to make rings from!
             if (poles[0].Count == 0) {
@@ -99,8 +105,6 @@ namespace WalkManifold.Internals {
             //then try to connect it to the next highest vertex, and continue until we
             //have considered all potential poles.
 
-            //PartialRing partialRing = new PartialRing(cel)
-            //partialRing.Cell = new int2(x, y);
             rings[0] = -1;
             rings[1] = -1;
             rings[2] = -1;
@@ -126,12 +130,9 @@ namespace WalkManifold.Internals {
 
               //Only consider the next vertex (which will always come below the previously
               //considered vertex) if there is at most LegHeight of distance between them.
-
-              //TODO: this allows a kind of "spiral staircase" situation where each vertex
-              //      is a full LegHeight below the previous, resulting in a single ring with
-              //      3x LegHeight vertical climb.  Maybe switch to using distance to the first
-              //      vertex instead of the previous?
               if ((prevHeight - nextHeight) > LegHeight) {
+                //We break instead of continue.  Rings are sorted by heights, so all following
+                //heights will be even lower and will never satisfy
                 break;
               }
 
@@ -145,66 +146,38 @@ namespace WalkManifold.Internals {
               prevHeight = nextHeight;
             }
 
-            PartialRing partialRing = new PartialRing() {
-              Cell = new int2(x, y)
-            };
+            PartialRing partialRing;
 
             int mask = 0;
             mask |= rings[0] == -1 ? 0 : 0b0001;
             mask |= rings[1] == -1 ? 0 : 0b0010;
             mask |= rings[2] == -1 ? 0 : 0b0100;
             mask |= rings[3] == -1 ? 0 : 0b1000;
-            switch (mask) {
-              default:
-              case 0b0000:
-                partialRing.Type = PartialRingType.Invalid;
-                break;
-              case 0b0001:
-              case 0b0010:
-              case 0b0100:
-              case 0b1000:
-                partialRing.Type = PartialRingType.Corner;
-                break;
-              case 0b0011:
-              case 0b0110:
-              case 0b1100:
-              case 0b1001:
-                partialRing.Type = PartialRingType.Edge;
-                break;
-              case 0b0101:
-              case 0b1010:
-                partialRing.Type = PartialRingType.Diagonal;
-                break;
-              case 0b1110:
-              case 0b1101:
-              case 0b1011:
-              case 0b0111:
-                partialRing.Type = PartialRingType.InvertedCorner;
-                break;
-              case 0b1111:
-                partialRing.Type = PartialRingType.Complete;
-                break;
-            }
+            partialRing.Type = MaskToPartialTypeLookup[mask];
+            partialRing.Cell = new int2(x, y);
 
+            //Align the ring based on the requirements of the PartialRing structure
+            //This makes it easier later to do the reconstruction logic, as partial
+            //rings are in an easy-to-reason-with state
             int Start = 0;
             for (int i = 0; i < 4; i++) {
-              if (rings[i] >= 0 && rings[(i + 3) % 4] == -1) {
+              if (rings[i] >= 0 && rings[(i + 3) & 3] == -1) {
                 Start = i;
                 break;
               }
             }
 
-            partialRing.V0 = rings[(Start + 0) % 4];
-            partialRing.P0 = LocalCoordinates[(Start + 0) % 4];
+            partialRing.V0 = rings[(Start + 0) & 3];
+            partialRing.P0 = LocalCoordinates[(Start + 0) & 3];
 
-            partialRing.V1 = rings[(Start + 1) % 4];
-            partialRing.P1 = LocalCoordinates[(Start + 1) % 4];
+            partialRing.V1 = rings[(Start + 1) & 3];
+            partialRing.P1 = LocalCoordinates[(Start + 1) & 3];
 
-            partialRing.V2 = rings[(Start + 2) % 4];
-            partialRing.P2 = LocalCoordinates[(Start + 2) % 4];
+            partialRing.V2 = rings[(Start + 2) & 3];
+            partialRing.P2 = LocalCoordinates[(Start + 2) & 3];
 
-            partialRing.V3 = rings[(Start + 3) % 4];
-            partialRing.P3 = LocalCoordinates[(Start + 3) % 4];
+            partialRing.V3 = rings[(Start + 3) & 3];
+            partialRing.P3 = LocalCoordinates[(Start + 3) & 3];
 
             //Complete rings don't get added to the partial ring list, just add them into the
             //complete ring list right away.  This has the added benefit that all square rings
